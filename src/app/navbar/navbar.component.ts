0f3a0c191a28dbde42d58e9072a5dc4581de4eaa
Jesus Emmanuel Meza Yarto <je.yg@hotmail.com>
2023-03-04 14:41:19 -0600

    front-navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isActive=false;
 
+  toggleClass(){
+    this.isActive=!this.isActive;
+  }
 }
