e9282606fab7a03cc0dc1bf7c6caa685f326e77a
Jesus Emmanuel Meza Yarto <je.yg@hotmail.com>
2023-03-05 00:01:39 -0600

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AdminComponent } from './admin/admin.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormReserComponent } from './form-reser/form-reser.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  
+  {path: 'admin', component: AdminComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about_us', component: AboutusComponent},
+  {path: 'reservations', component: FormReserComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: LogoutComponent}
 
 ];
 
